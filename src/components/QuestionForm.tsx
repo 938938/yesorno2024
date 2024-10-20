598dc1f3e3d932852f1c31732265917555d64cd3
938938 <938kjh@gmail.com>
2024-10-20 23:34:19 +0900

    [FEAT] 로딩 기능 구현

--- src/components/QuestionForm.tsx
+++ src/components/QuestionForm.tsx
@@ -11,27 +11,47 @@ const QuestionForm = () => {
     answer: '',
     image: '',
   });
-  const answerForm = async (formData: FormData) => {
-    const question = formData.get('question')!.toString();
-    const res = await fetch(`https://yesno.wtf/api`);
-    if (!res.ok) {
-      throw Error(res.statusText);
+  const [loading, setLoading] = useState<boolean>(true);
+  const getAnswer = async () => {
+    try {
+      const res = await fetch(`https://yesno.wtf/api`);
+      if (!res.ok) {
+        throw Error(res.statusText);
+      }
+      const answer = await res.json();
+      if (
+        answer.image ===
+        'https://yesno.wtf/assets/no/3-80a6f5b5d6684674bcfeda34accca4e1.gif'
+      ) {
+        answer.image =
+          'https://yesno.wtf/assets/no/0-b6d3e555af2c09094def76cf2fbddf46.gif';
+      }
+      setAnswer(answer);
+    } catch (error) {
+      console.error('Failed to fetch answer:', error);
+    } finally {
+      setLoading(false);
     }
-    const answer = await res.json();
+  };
+  const handleAnswerForm = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const question = formData.get('question')!.toString();
+    setLoading(true);
     setQuestion(question);
-    setAnswer(answer);
+    await getAnswer();
   };
 
   return (
     <div>
-      <form action={answerForm}>
+      <form onSubmit={handleAnswerForm}>
         <input name='question' />
         <button type='submit'>클릭</button>
       </form>
       <div>
         <p>{question}</p>
         <p>{answer.answer}</p>
-        {answer.image === '' ? (
+        {loading ? (
           <div className={style.loader} />
         ) : (
           <Image
@@ -39,8 +59,8 @@ const QuestionForm = () => {
             alt='결과 이미지'
             height={300}
             width={400}
+            unoptimized={true}
           />
-          // <div className={style.loader} />
         )}
       </div>
     </div>
